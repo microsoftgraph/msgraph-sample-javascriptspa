db75f6f0721e12401daa98835e6f463301d60935
Jason Johnston <jasonjoh@microsoft.com>
2021-08-02 11:28:48 -0400

    Refactored update to use AuthCodeMSALBrowserAuthenticationProvider

--- demo/graph-tutorial/auth.js
+++ demo/graph-tutorial/auth.js
@@ -14,8 +14,9 @@ async function signIn() {
     // Use MSAL to login
     const authResult = await msalClient.loginPopup(msalRequest);
     console.log('id_token acquired at: ' + new Date().toString());
-    // Save the account username, needed for token acquisition
-    sessionStorage.setItem('msalAccount', authResult.account.username);
+
+    // Initialize the Graph client
+    initializeGraphClient(msalClient, authResult.account, msalRequest.scopes);
 
     // Get the user's profile from Graph
     user = await getUser();
@@ -34,38 +35,7 @@ async function signIn() {
 
 // <signOutSnippet>
 function signOut() {
-  account = null;
   sessionStorage.removeItem('graphUser');
   msalClient.logout();
 }
 // </signOutSnippet>
-
-// <getTokenSnippet>
-async function getToken() {
-  let account = sessionStorage.getItem('msalAccount');
-  if (!account){
-    throw new Error(
-      'User account missing from session. Please sign out and sign in again.');
-  }
-
-  try {
-    // First, attempt to get the token silently
-    const silentRequest = {
-      scopes: msalRequest.scopes,
-      account: msalClient.getAccountByUsername(account)
-    };
-
-    const silentResult = await msalClient.acquireTokenSilent(silentRequest);
-    return silentResult.accessToken;
-  } catch (silentError) {
-    // If silent requests fails with InteractionRequiredAuthError,
-    // attempt to get the token interactively
-    if (silentError instanceof msal.InteractionRequiredAuthError) {
-      const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
-      return interactiveResult.accessToken;
-    } else {
-      throw silentError;
-    }
-  }
-}
-// </getTokenSnippet>
